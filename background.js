a9ffa155dad8e1322c17523319dbdb06e9e46639
Zacqary Adam Xeper <zxeper@fastcompany.com>
2015-05-22 13:09:31 -0400

    Extension will now redirect a hostname to an IP/other host name

--- background.js
+++ background.js
@@ -1,13 +1,35 @@
 var vhost = "";
+var ip = "";
 var enabled = false;
+var requestsToRemap = {};
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details){
+		if (enabled) {
+			var domain = details.url.match(/^(?:https?:\/\/)?([^\/:]+)/i)[1];
+			if (domain === vhost){
+				var redirectUrl = details.url.replace(vhost, ip);
+				requestsToRemap[redirectUrl] = vhost;
+				return {redirectUrl: redirectUrl};
+			}	
+		}
+	},
+	{urls: ["<all_urls>"]},
+	["blocking"]
+);
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	function(details) {
-		if (enabled)
+		console.log(details.url);
+		if (enabled && requestsToRemap[details.url])
 		{
-			details.requestHeaders.push({ name: "Host", value: vhost });
+			var remap = requestsToRemap[details.url];
+			details.requestHeaders.push({ name: "Host", value: remap });
+			delete requestsToRemap[details.url];
+			console.log(details.requestHeaders);
 			return {requestHeaders: details.requestHeaders};
 		}
 	},
 	{urls: ["<all_urls>"]},
-	["blocking", "requestHeaders"]);
+	["blocking", "requestHeaders"]
+);
--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 window.addEventListener("load", function(e)
 {
+	var ip = document.querySelector('#ip_address');
 	var vn = document.querySelector('#vhost_name');
+	ip.addEventListener("keyup", update);
 	vn.addEventListener("keyup", update);
+	ip.value = chrome.extension.getBackgroundPage().ip;
 	vn.value = chrome.extension.getBackgroundPage().vhost;
 	var ve = document.querySelector('#vhost_enable');
 	ve.addEventListener("change", tick);
@@ -11,14 +14,14 @@ window.addEventListener("load", function(e)
 
 function update(e)
 {
-	chrome.extension.getBackgroundPage().vhost = e.target.value;
+	chrome.extension.getBackgroundPage()[e.target.name] = e.target.value;
 	chrome.extension.getBackgroundPage().enabled = true;
 	document.getElementById("vhost_enable").checked = true;
-	chrome.browserAction.setIcon({path: 'enabled.png'})
+	chrome.browserAction.setIcon({path: 'enabled.png'});
 }
 
 function tick(e)
 {
 	chrome.extension.getBackgroundPage().enabled = e.target.checked;
-	chrome.browserAction.setIcon({path: (e.target.checked ? 'enabled' : 'disabled') + '.png'})
+	chrome.browserAction.setIcon({path: (e.target.checked ? 'enabled' : 'disabled') + '.png'});
 }
